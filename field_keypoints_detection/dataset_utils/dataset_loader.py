Augmentation and Rescaling/Normalization fixes. Minor metrics changes. Updated the KeypointModel class. Moved keypoint visualization to a dedicated file.

field_keypoints_detection/dataset_utils/dataset_loader.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from functools import partial
 
 def _parse_and_decode_function(example_proto):
     feature_description = {
@@ -16,7 +17,24 @@ def _parse_and_decode_function(example_proto):
 
     return image, keypoints
 
-def load_tfrecords(fpath):
-    raw_dataset = tf.data.TFRecordDataset(fpath)
-    parsed_and_decoded_dataset = raw_dataset.map(_parse_and_decode_function, num_parallel_calls=tf.data.AUTOTUNE)
-    return parsed_and_decoded_dataset
+def load_dataset(filenames):
+    ignore_order = tf.data.Options()
+    ignore_order.experimental_deterministic = False  # disable order, increase speed
+    dataset = tf.data.TFRecordDataset(
+        filenames
+    )  # automatically interleaves reads from multiple files
+    dataset = dataset.with_options(
+        ignore_order
+    )  # uses data as soon as it streams in, rather than in its original order
+    dataset = dataset.map(
+        partial(_parse_and_decode_function), num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+    # returns a dataset of (image, label) pairs if labeled=True or just images if labeled=False
+    return dataset
+
+def get_dataset(filenames, batch_size):
+    dataset = load_dataset(filenames)
+    dataset = dataset.shuffle(2048)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.batch(batch_size)
+    return dataset

field_keypoints_detection/metrics.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 
 class VisibleKeypointMSE(tf.keras.metrics.Metric):
@@ -36,10 +35,11 @@ class VisibilityAccuracy(tf.keras.metrics.Metric):
         y_pred_visibility = tf.cast(tf.round(y_pred[:, :, 2]), dtype=tf.int32)
 
         correct = tf.reduce_sum(tf.cast(tf.equal(y_true_visibility, y_pred_visibility), dtype=tf.float32))
-        total = tf.size(y_true_visibility, out_type=tf.float32)
+        total = tf.cast(tf.size(y_true_visibility, out_type=tf.int32), tf.float32)  # Corrected line
 
         self.correct.assign_add(correct)
         self.total.assign_add(total)
+
 
     def result(self):
         return self.correct / self.total

field_keypoints_detection/model.py
@@ -1,49 +1,63 @@
 import tensorflow as tf
-from dataset_utils import load_tfrecords
-from field_keypoints_detection.data_augmentation import ResizeAndNormalizeLayer, AugmentationLayer
+from dataset_utils import get_dataset
+from field_keypoints_detection.data_preprocessing import ResizeAndNormalize, Augmentation
 from field_keypoints_detection.loss import visibility_based_mse
 from field_keypoints_detection.metrics import VisibilityAccuracy, VisibleKeypointMSE
 
 class KeypointModel:
-    def __init__(self, train_tfrecord_path, val_tfrecord_path, test_tfrecord_path, batch_size=32):
-        # Loading TFRecords
-        train_dataset =  load_tfrecords(train_tfrecord_path)
-        val_dataset =  load_tfrecords(val_tfrecord_path)
-        test_dataset = load_tfrecords(test_tfrecord_path)
+    def __init__(self, train_tfrecord_path, val_tfrecord_path, test_tfrecord_path, num_keypoints, batch_size=32):
+        self.num_keypoints = num_keypoints
+        self.batch_size = batch_size
 
-        # Batching
-        train_dataset = train_dataset.batch(batch_size)
-        val_dataset = val_dataset.batch(batch_size)
-        test_dataset = test_dataset.batch(batch_size)
+        # Initialize preprocessing and augmentation
+        self.resize_and_normalize = ResizeAndNormalize(target_size=(300, 300))
+        self.augmentation = Augmentation(rescale_size=(300, 300))
 
-        # Prefetching
-        train_dataset =  train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-        val_dataset =  val_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-        test_dataset =  test_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+        # Create datasets
+        self.train_dataset = self.load_and_preprocess_dataset(train_tfrecord_path, training=True)
+        self.val_dataset = self.load_and_preprocess_dataset(val_tfrecord_path)
+        self.test_dataset = self.load_and_preprocess_dataset(test_tfrecord_path)
 
-        self.train_dataset = train_dataset
-        self.val_dataset = val_dataset
-        self.test_dataset = test_dataset
+
+    def load_and_preprocess_dataset(self, tfrecord_path, training=False):
+        dataset = get_dataset(tfrecord_path, self.batch_size)
+        dataset = dataset.map(lambda image, keypoints: self.preprocess(image, keypoints, training=training),
+                              num_parallel_calls=tf.data.AUTOTUNE)
+        return dataset
+
+    def preprocess(self, image, keypoints, training=False):
+        
+        # Reshape
+        keypoints_shape = tf.shape(keypoints)
+        if len(keypoints_shape) == 2:
+          keypoints = tf.expand_dims(keypoints, axis=0)  # Add batch dimension if missing
+
+        # Reshape keypoints to ensure they have the shape (batch_size, 32, 3)
+        keypoints = tf.reshape(keypoints, [-1, 32, 3])
+
+        # Apply augmentation (only during training)
+        if training:
+            image, keypoints = self.augmentation.apply(image, keypoints)
+
+        # Apply resizing and normalization
+        image, keypoints = self.resize_and_normalize.apply(image, keypoints)
+
+        return image, keypoints
 
     def build(self):
-        input_image = tf.keras.layers.Input(shape=(None, None, 3))
-        input_keypoints = tf.keras.layers.Input(shape=(None, 3)) 
+        # Define input layers
+        image_input = tf.keras.layers.Input(shape=(None, None, 3), dtype=tf.float32)
 
-        # Get the number of keypoints dynamically
-        num_keypoints = tf.shape(input_keypoints)[1]
-
-        # Apply resize and normalization layer
-        resized_image, normalized_keypoints = ResizeAndNormalizeLayer(target_size=(300, 300))([input_image, input_keypoints])
-
-        # Apply augmentation layer (only during training)
-        augmented_image, augmented_keypoints = AugmentationLayer()([resized_image, normalized_keypoints])
+        x = tf.keras.applications.efficientnet.preprocess_input(image_input)
 
         # Use EfficientNetB3 as feature extractor
-        base_model = tf.keras.applications.EfficientNetB3(include_top=False, input_tensor=augmented_image)
+        base_model = tf.keras.applications.EfficientNetB3(include_top=False, input_tensor=image_input)
         base_model.trainable = False  # Freeze the base model
 
+        x = base_model(x)
+
         # Add custom layers on top
-        x = tf.keras.layers.Conv2D(256, (5, 5), activation='relu', padding='same')(base_model.output)
+        x = tf.keras.layers.Conv2D(256, (5, 5), activation='relu', padding='same')(x)
         x = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
         x = tf.keras.layers.MaxPooling2D((2, 2))(x)
         x = tf.keras.layers.Conv2D(64, (1, 1), activation='relu', padding='same')(x)
@@ -51,21 +65,23 @@ class KeypointModel:
         x = tf.keras.layers.Dropout(0.5)(x)
         x = tf.keras.layers.Dense(512, activation='relu')(x)
         x = tf.keras.layers.Dropout(0.5)(x)
-        output = tf.keras.layers.Dense(num_keypoints * 3, activation='linear')(x)
-        output = tf.keras.layers.Reshape((num_keypoints, 3))(output)  
+        output = tf.keras.layers.Dense(self.num_keypoints * 3, activation='linear')(x)
+        output = tf.keras.layers.Reshape((self.num_keypoints, 3))(output)
 
-        model = tf.keras.Model(inputs=[input_image, input_keypoints], outputs=[output])
+        # Create the model
+        self.model = tf.keras.Model(inputs=[image_input], outputs=output)
 
-        model.compile(optimizer='adam', loss=visibility_based_mse,
-                      metrics=[VisibleKeypointMSE(), VisibilityAccuracy()])
-
-        self.model = model
-
+        # Compile the model
+        self.model.compile(optimizer='adam', loss=visibility_based_mse(),
+                          metrics=[VisibleKeypointMSE(), VisibilityAccuracy()])
 
     def train(self, epochs=10):
+        self.training = True
         # Train the model
         self.model.fit(self.train_dataset, epochs=epochs, validation_data=self.val_dataset)
 
     def evaluate(self):
+        self.training = False
         # Evaluate the model
         self.model.evaluate(self.test_dataset)
+